77e20e01def0c445679c533bf78c130dc31b63f1
iqbaliamal <iqbaliamal@gmail.com>
2021-03-02 23:46:08 +0700

    menu sidebar

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -37,48 +37,43 @@
           </div>
           <ul class="sidebar-menu">
               <li class="menu-header">Dashboard</li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                  <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-                </ul>
+              <li class="">
+                <a href="/admin" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
               </li>
               <li class="menu-header">Main</li>
-              <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
               <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Kategori</span></a>
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Blog</span></a>
                 <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="{{route('list.kategori')}}">List Kategori</a></li>
-                  <li><a class="nav-link" href="Kategori-transparent.html">Transparent Sidebar</a></li>
-                  <li><a class="nav-link" href="Kategori-top-navigation.html">Top Navigation</a></li>
+                  <li><a class="nav-link" href="">Artikel</a></li>
+                  <li><a class="nav-link" href="">Komentar</a></li>
+                  <li><a class="nav-link" href="{{route('list.kategori')}}">Kategori</a></li>
                 </ul>
               </li>
-              <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+              <li class="">
+                <a class="nav-link" href=""><i class="fas fa-users"></i><span>Pengurus</span></a>
+              </li>
+              <li class="">
+                <a class="nav-link" href=""><i class="fas fa-comment-alt"></i><span>Kritik dan Saran</span></a>
+              </li>
+              <li class="">
+                <a class="nav-link" href=""><i class="fas fa-share-alt-square"></i><span>Google Form</span></a>
+              </li>
               <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Data Master</span></a>
                 <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
-                  <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
-                  <li><a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a></li>
-                  <li><a class="nav-link" href="bootstrap-buttons.html">Buttons</a></li>
-                  <li><a class="nav-link" href="bootstrap-card.html">Card</a></li>
-                  <li><a class="nav-link" href="bootstrap-carousel.html">Carousel</a></li>
-                  <li><a class="nav-link" href="bootstrap-collapse.html">Collapse</a></li>
-                  <li><a class="nav-link" href="bootstrap-dropdown.html">Dropdown</a></li>
-                  <li><a class="nav-link" href="bootstrap-form.html">Form</a></li>
-                  <li><a class="nav-link" href="bootstrap-list-group.html">List Group</a></li>
-                  <li><a class="nav-link" href="bootstrap-media-object.html">Media Object</a></li>
-                  <li><a class="nav-link" href="bootstrap-modal.html">Modal</a></li>
-                  <li><a class="nav-link" href="bootstrap-nav.html">Nav</a></li>
-                  <li><a class="nav-link" href="bootstrap-navbar.html">Navbar</a></li>
-                  <li><a class="nav-link" href="bootstrap-pagination.html">Pagination</a></li>
-                  <li><a class="nav-link" href="bootstrap-popover.html">Popover</a></li>
-                  <li><a class="nav-link" href="bootstrap-progress.html">Progress</a></li>
-                  <li><a class="nav-link" href="bootstrap-table.html">Table</a></li>
-                  <li><a class="nav-link" href="bootstrap-tooltip.html">Tooltip</a></li>
-                  <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
+                  <li><a class="nav-link" href="">Departemen</a></li>
+                  <li><a class="nav-link" href="">Biro</a></li>
+                  <li><a class="nav-link" href="">Jabatan</a></li>
+                  <li><a class="nav-link" href="">Prodi</a></li>
+                  <li><a class="nav-link" href="">Golongan</a></li>
+                  <li><a class="nav-link" href="">Angkatan</a></li>
                 </ul>
+              </li>
+              <li class="">
+                <a class="nav-link" href=""><i class="fas fa-user"></i><span>Users / Admin</span></a>
+              </li>
+              <li class="">
+                <a class="nav-link" href=""><i class="fas fa-address-card"></i><span>Edit Profile</span></a>
               </li>
               {{-- <li class="menu-header">Stisla</li>
               <li class="nav-item dropdown">
