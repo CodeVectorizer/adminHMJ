e0d25a2050ae59fe01bb61eda797957a92a67acb
CodeVectorizer <lukmanafandi10@gmail.com>
2021-03-20 22:16:51 +0700

    fix

--- resources/views/user/pages/blog-kategori.blade.php
+++ resources/views/user/pages/blog-kategori.blade.php
@@ -32,7 +32,7 @@
                             </div>
                             <h5 class="card-title">{{ $artikel->judul}}</a></h5>
                             <p class="card-text pt-1">{{ Str::substr($artikel->isi, 0, 250) }}........</p>
-                            <a href="blog/{{ $artikel->id_artikel }}" class="card-foot">Read More</a>
+                            <a href="blog/{{ $artikel->slug }}" class="card-foot">Read More</a>
                         </div>
                     </div>
                 </div>
